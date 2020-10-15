9e0f02105e4950910049bde6ce936344cb33e12c
Eveliina N <72910612+evelinaanne@users.noreply.github.com>
2020-10-15 12:51:34 +0300

    Add files via upload
    Practice with pop-up in Xamarin.Forms. 
    
    Used https://xamarinmonkeys.blogspot.com/2018/05/xamarinforms-custom-popup.html as a source for this exercise

---
+++ MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Popup
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private void Iampopup(object sender, EventArgs e)
+        {
+            popupContent.IsVisible = true;
+        }
+
+        private void Popupends(object sender, EventArgs e)
+        {
+            popupContent.IsVisible = false;
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+
+
+    }
+
+}
